0c7513660e1025fda2fb38ea98e9c035ef3259c8
sk261 <sk261@humboldt.edu>
2020-01-28 17:57:00 -0800

    Added click event on deck, made a rough idea of the player's card.

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public GameObject Deck;
+    public GameObject Card;
+    private List<GameObject> Cards;
+    /* TODO:
+     * Make draggable cards.
+     * Make drawable cards.
+     * */
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cards = new List<GameObject>();
+    }
+
+    void DrawFromDeck()
+    {
+        GameObject NewCard = Instantiate(Card);
+        NewCard.SetActive(true);
+        Cards.Add(NewCard);
+        print("Card drawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            switch (hit.collider.name)
+            {
+                case "Deck":
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        DrawFromDeck();
+                    }
+                    break;
+            }
+        }
+    }
+}
